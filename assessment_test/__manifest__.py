48de3a3382b85ccda5108e1aa743196bbd61e9bf
Diogo Duarte <diogocarvalhoduarte@gmail.com>
2017-09-28 08:23:34 +0100

    Remove module

---
+++ assessment_test/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Assessment Test',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Assessment Test',
+    'description': """
+This module adds Assessment Test
+""",
+    'depends': ['sale'],
+    'data': [
+        # 'views/map_templates.xml',
+        # 'views/partner_view.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
